48fe14d2c628b80aa49ee4672c410b51af377ea0
Thiago Moreira de Souza Arrais <thiagomds.scientist@gmail.com>
2020-03-23 16:26:56 -0300

    criando a classe ClienteRepository

---
+++ src/TMSA.RentCar.Cadastro.Data/Repository/ClienteRepository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using TMSA.RentCar.Cadastro.Data.Context;
+using TMSA.RentCar.Cadastro.Domain.Clientes;
+using TMSA.RentCar.Cadastro.Domain.Clientes.Interfaces;
+
+namespace TMSA.RentCar.Cadastro.Data.Repository
+{
+    public class ClienteRepository : IClienteRepository
+    {
+        private readonly ClienteContext _db;
+
+        public ClienteRepository(ClienteContext db)
+        {
+            _db = db;
+        }
+
+        public void Adicionar(Cliente cliente)
+        {
+            _db.Clientes.Add(cliente);
+        }
+
+        public void Atualizar(Cliente cliente)
+        {
+            _db.Clientes.Update(cliente);
+        }
+
+        public Cliente ObterPorCpf(string cpf)
+        {
+            return _db.Clientes.FirstOrDefault(c => c.CPF == cpf);
+        }
+
+        public Cliente ObterPorEmail(string email)
+        {
+            return _db.Clientes.FirstOrDefault(c => c.Email == email);
+        }
+
+        public Cliente ObterPorId(Guid clienteId)
+        {
+            return _db.Clientes.Find(clienteId);
+        }
+    }
+}
